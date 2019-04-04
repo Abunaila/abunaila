c12761c3b8b2711efa87bb6ea304d144f69db52f
Abunaila <45642023+Abunaila@users.noreply.github.com>
2019-04-05 02:53:22 +0300

    Add files via upload

---
+++ phoenix_freetv_HOME.php
@@ -0,0 +1,345 @@
+
+
+<item>
+<title>[COLOR dodgerblue]Donacije za razvoj i podršku:[/COLOR][COLOR orangered] paypal.me/kodiphoenixreborn[/COLOR]<title>
+<thumbnail>https://www.vodafone.de/media/img/teaser/170413-paypal-08-325x226-dt-s.jpg</thumbnail>
+<fanart>https://www.vodafone.de/media/img/teaser/170413-paypal-08-325x226-dt-s.jpg</fanart>
+<link>plugin://plugin.program.chrome.launcher/?kiosk=yes&amp;url=https://goo.gl/Nz9ALW&amp;mode=showSite&amp;stopPlayback=yes</link>
+<item>
+http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/HOME.txt
+<item>
+<title>[COLOR dodgerblue]Facebook:[/COLOR][COLOR orangered] Kodi Phoenix Reborn[/COLOR]<title>
+<thumbnail>https://www.facebook.com/images/fb_icon_325x325.png</thumbnail>
+<fanart>https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Facebook_New_Logo_%282015%29.svg/2000px-Facebook_New_Logo_%282015%29.svg.png</fanart>
+<link>plugin://plugin.program.chrome.launcher/?kiosk=yes&amp;url=https://tinyurl.com/ybyqwrwz&amp;mode=showSite&amp;stopPlayback=yes</link>
+<item>
+
+<item>
+<title>[COLOR crimson]=============[/COLOR][COLOR deepskyblue] LIVE [/COLOR][COLOR crimson]=============[/COLOR]<title>
+<thumbnail>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</thumbnail>
+<fanart>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</fanart>
+<link>https://goo.gl/Nz9ALW&amp;mode=showSite&amp;stopPlayback=yes</link>
+<item>
+
+<item>
+<title>[COLOR lime]Sportski događaji uživo[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://images-na.ssl-images-amazon.com/images/I/81wxZizpvsL.png</thumbnail>
+<fanart>http://www.racunalo.com/wp-content/uploads/2018/06/Hrvatska-Argentina-live-stream-u%C5%BEivo-danas-gledajte-nogometnu-utakmicu-2018-FIFA-World-Cup-svjetsko-nogometno-prvenstvo-01.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/sportskidogadajiuzivo.xml</externallink>
+<item>
+
+<item>
+<title>[COLOR crimson]=============[/COLOR][COLOR deepskyblue] EX - YU [/COLOR][COLOR crimson]=============[/COLOR]<title>
+<thumbnail>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</thumbnail>
+<fanart>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</fanart>
+<link>https://goo.gl/Nz9ALW&amp;mode=showSite&amp;stopPlayback=yes</link>
+<item>
+
+<item>
+<title>[COLOR dodgerblue]B[/COLOR][COLOR white]iH TV Programi[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>http://www.zoplanet.com.hr/wp-content/uploads/2017/08/bosna-herzegovina-flag.jpg</thumbnail>
+<fanart>http://www.zoplanet.com.hr/wp-content/uploads/2017/08/bosna-herzegovina-flag.jpg</fanart>
+<externallink>
+http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/bosanskiprogrami.txt
+</externallink>
+<item>
+
+<item>
+<title>[COLOR dodgerblue]D[/COLOR][COLOR white]okumentarni TV Programi[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://cdni.rt.com/files/2015.12/article/5661d7dac3618841498b45d4.png</thumbnail>
+<fanart>https://cdni.rt.com/files/2015.12/article/5661d7dac3618841498b45d4.png</fanart>
+<externallink>
+http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/dokumentarniprogrami.xml
+</externallink>
+<item>
+
+<item>
+<title>[COLOR dodgerblue]F[/COLOR][COLOR white]ilmski TV Programi[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://2.bp.blogspot.com/-S__8zsdXh_g/TyaDMr09tjI/AAAAAAAAAEc/wJbi0HuYbQM/s1600/dish_network_channels.jpg</thumbnail>
+<fanart>https://2.bp.blogspot.com/-S__8zsdXh_g/TyaDMr09tjI/AAAAAAAAAEc/wJbi0HuYbQM/s1600/dish_network_channels.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/filmskiprogrami.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR dodgerblue]H[/COLOR][COLOR white]rvatski TV Programi[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>http://www.brotnjo.info/wp-content/uploads/2018/07/zastava.jpg</thumbnail>
+<fanart>http://www.brotnjo.info/wp-content/uploads/2018/07/zastava.jpg</fanart>
+<externallink>
+http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/hrvatskiprogrami.txt
+</externallink>
+<item>
+
+<item>
+<title>[COLOR dodgerblue]I[/COLOR][COLOR white]nformativni TV Program[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>http://hr.n1info.com/Picture/87644/jpeg/n121.jpg</thumbnail>
+<fanart>http://hr.n1info.com/Picture/87644/jpeg/n121.jpg</fanart>
+<externallink>
+http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/informativniprogrami.txt
+</externallink>
+<item>
+
+<item>
+<title>[COLOR dodgerblue]M[/COLOR][COLOR white]akedonski TV Programi[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://ak4.picdn.net/shutterstock/videos/5460224/thumb/1.jpg</thumbnail>
+<fanart>https://ak4.picdn.net/shutterstock/videos/5460224/thumb/1.jpg</fanart>
+<externallink>
+http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/makedonskiprogrami.txt
+</externallink>
+<item>
+
+<item>
+<title>[COLOR dodgerblue]M[/COLOR][COLOR white]uzički TV Programi - EX-YU[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>http://www.ballroomguide.com/images/music.jpg</thumbnail>
+<fanart>http://www.ballroomguide.com/images/music.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/muzika-ex-yu.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR dodgerblue]P[/COLOR][COLOR white]ink TV Programi[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>http://www.sat-multimedia.com/wp-content/uploads/2015/04/pinkTV.jpg</thumbnail>
+<fanart>http://www.sat-multimedia.com/wp-content/uploads/2015/04/pinkTV.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/pinkprogrami.txt</externallink>
+
+<item>
+<title>[COLOR dodgerblue]S[/COLOR][COLOR white]lovenski TV Programi[/COLOR][COLOR crimson] /Bikerdays Lista/[/COLOR][COLOR gold] Potrebna HR, SR ili NL ip adresa[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>http://s1.1zoom.me/big0/936/331942-alexfas01.jpg</thumbnail>
+<fanart>http://s1.1zoom.me/big0/936/331942-alexfas01.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/slovenskiprogrami.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR dodgerblue]S[/COLOR][COLOR white]portski TV Programi - Domaći[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Logo_Sport.svg/1280px-Logo_Sport.svg.png</thumbnail>
+<fanart>https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Logo_Sport.svg/1280px-Logo_Sport.svg.png</fanart>
+<externallink>
+http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/sportskiprogrami.xml
+</externallink>
+<item>
+
+<item>
+<title>[COLOR dodgerblue]S[/COLOR][COLOR white]rpski TV Programi[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://img1.goodfon.com/wallpaper/big/d/ae/serbiya-flag-serbia.jpg</thumbnail>
+<fanart>https://img1.goodfon.com/wallpaper/big/d/ae/serbiya-flag-serbia.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/srpskiprogrami.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR lightslategrey]----------------------------------------------------------------------[/COLOR]<title>
+<thumbnail>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</thumbnail>
+<fanart>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</fanart>
+<link>https://goo.gl/Nz9ALW&amp;mode=showSite&amp;stopPlayback=yes</link>
+<item>
+
+<item>
+<title>[COLOR crimson]<<<<[/COLOR][COLOR deepskyblue] ONLINE RADIO [/COLOR][COLOR crimson]>>>>[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>http://nikofeijo.files.wordpress.com/2011/05/radio-on-line.gif</thumbnail>
+<fanart>http://nikofeijo.files.wordpress.com/2011/05/radio-on-line.gif</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/radio.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR lightslategrey]----------------------------------------------------------------------[/COLOR]<title>
+<thumbnail>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</thumbnail>
+<fanart>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</fanart>
+<link>https://goo.gl/Nz9ALW&amp;mode=showSite&amp;stopPlayback=yes</link>
+<item>
+
+<item>
+<title>[COLOR crimson]=============[/COLOR][COLOR deepskyblue] WORLD [/COLOR][COLOR crimson]=============[/COLOR]<title>
+<thumbnail>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</thumbnail>
+<fanart>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</fanart>
+<link>https://goo.gl/Nz9ALW&amp;mode=showSite&amp;stopPlayback=yes</link>
+<item>
+
+<item>
+<title>[COLOR orangered]A[/COLOR][COLOR white]lbanian TV Channels[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://www.motosha.com/wp-content/uploads/albanian-flag-1024x569.jpg</thumbnail>
+<fanart>https://www.motosha.com/wp-content/uploads/albanian-flag-1024x569.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/albania.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR orangered]F[/COLOR][COLOR white]rench TV Channels[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://www.worldatlas.com/r/w728-h425-c728x425/upload/41/46/c2/untitled-design-277.jpg</thumbnail>
+<fanart>https://www.worldatlas.com/r/w728-h425-c728x425/upload/41/46/c2/untitled-design-277.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/francuskiprogrami.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR orangered]G[/COLOR][COLOR white]erman TV Channels[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>http://mamyste.lt/wp-content/uploads/2016/11/flag-germany-1060305_960_720.jpg</thumbnail>
+<fanart>http://mamyste.lt/wp-content/uploads/2016/11/flag-germany-1060305_960_720.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/njemackiprogrami.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR orangered]M[/COLOR][COLOR white]usic TV Channels - World[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>http://www.ballroomguide.com/images/music.jpg</thumbnail>
+<fanart>http://www.ballroomguide.com/images/music.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/stranamuzika.xml</externallink>
+<item>
+
+<item>
+<title>[COLOR orangered]I[/COLOR][COLOR white]talian TV Channels[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://www.gss-media.com/portal/wp-content/uploads/2016/12/Italija-Zastava.jpg</thumbnail>
+<fanart>https://www.gss-media.com/portal/wp-content/uploads/2016/12/Italija-Zastava.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/talijanskiprogrami.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR orangered]P[/COLOR][COLOR white]olish TV Channels[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://ak8.picdn.net/shutterstock/videos/762433/thumb/1.jpg</thumbnail>
+<fanart>https://ak8.picdn.net/shutterstock/videos/762433/thumb/1.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/poland.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR orangered]P[/COLOR][COLOR white]ortugese TV Channels[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://ak6.picdn.net/shutterstock/videos/6567476/thumb/1.jpg?i10c=img.resize(height:160)</thumbnail>
+<fanart>https://ak6.picdn.net/shutterstock/videos/6567476/thumb/1.jpg?i10c=img.resize(height:160)</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/portugal.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR orangered]R[/COLOR][COLOR white]omanian TV Channels[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://mediavideo.blastingnews.com/p/4/2017/11/11/bb7339bb-b1f1-4686-9059-f4a611f42d6a.jpg</thumbnail>
+<fanart>https://mediavideo.blastingnews.com/p/4/2017/11/11/bb7339bb-b1f1-4686-9059-f4a611f42d6a.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/romania.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR orangered]R[/COLOR][COLOR white]ussian TV Channels[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>http://mppre.gob.ve/wp-content/uploads/2018/05/bandera-de-Mosc%C3%BA.jpg</thumbnail>
+<fanart>http://mppre.gob.ve/wp-content/uploads/2018/05/bandera-de-Mosc%C3%BA.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/rusija.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR orangered]S[/COLOR][COLOR white]panish TV Channels[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://ak2.picdn.net/shutterstock/videos/2694782/thumb/1.jpg</thumbnail>
+<fanart>https://ak2.picdn.net/shutterstock/videos/2694782/thumb/1.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/spain.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR orangered]T[/COLOR][COLOR white]urkish TV Channels[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://www.agriorbit.com/wp-content/uploads/2018/10/turkiye-nin-tanitiminda-turkey-yerine-turkish-kullanilacak-1539258503.jpg</thumbnail>
+<fanart>https://www.agriorbit.com/wp-content/uploads/2018/10/turkiye-nin-tanitiminda-turkey-yerine-turkish-kullanilacak-1539258503.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/turkish.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR orangered]U[/COLOR][COLOR white]K/US TV Channels[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>http://agnewsfeed.com/here/wp-content/uploads/2017/02/US-UK-trade-deal-could-take-place-in-two-years_strict_xxl.jpg</thumbnail>
+<fanart>http://agnewsfeed.com/here/wp-content/uploads/2017/02/US-UK-trade-deal-could-take-place-in-two-years_strict_xxl.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/ukusprogrami.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR orangered]W[/COLOR][COLOR white]orld Sport TV Channels[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Logo_Sport.svg/1280px-Logo_Sport.svg.png</thumbnail>
+<fanart>https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Logo_Sport.svg/1280px-Logo_Sport.svg.png</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/stranisportskiprogrami.txt</externallink>
+<item>
+
+
+<item>
+<title>[COLOR crimson]=============[/COLOR][COLOR deepskyblue] Ostali Addoni Potrebno imati instaliran phoenixreborn repozitorij [/COLOR][COLOR crimson]=============[/COLOR]<title>
+<thumbnail>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</thumbnail>
+<fanart>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</fanart>
+<link>https://goo.gl/Nz9ALW&amp;mode=showSite&amp;stopPlayback=yes</link>
+<item>
+
+<item>
+<title>[COLOR orangered]Phoenix Reborn[/COLOR] [COLOR dodgerblue]Movies[/COLOR] [COLOR white]Addon[/COLOR]</title>
+<thumbnail>http://cdn.phoenixrebornbuild.com.hr/slike-phoenix/phoenix_reborn_movies_logo.png</thumbnail>
+<fanart>http://cdn.phoenixrebornbuild.com.hr/slike-phoenix/phoenix_reborn_movies_logobackground.jpg</fanart>
+<link>http://ignoreme</link>
+<jsonrpc>plugin://plugin.video.phoenixrebornmovies</jsonrpc>
+</item>
+</items>
+
+<item>
+<title>[COLOR orangered]Phoenix[/COLOR] [COLOR cyan]k[/COLOR][COLOR magenta]I[/COLOR][COLOR lime]d[/COLOR][COLOR yellow]S[/COLOR] [COLOR white]Addon[/COLOR]</title>
+<thumbnail>http://cdn.phoenixrebornbuild.com.hr/slike-phoenix/phoenix_kids_ikona.png</thumbnail>
+<fanart>http://cdn.phoenixrebornbuild.com.hr/slike-phoenix/phoenix_kids_background.jpg</fanart>
+<link>http://ignoreme</link>
+<jsonrpc>plugin://plugin.video.phoenixkids</jsonrpc>
+</item>
+</items>
+
+<item>
+<title>[COLOR orangered]Phoeni[/COLOR][COLOR magenta]xxx[/COLOR] [COLOR orangered]Reborn[/COLOR] [COLOR magenta]Porn[/COLOR] [COLOR white]Addon[/COLOR]</title>
+<thumbnail>http://cdn.phoenixrebornbuild.com.hr/slike-phoenix/_phoenix-adult.png</thumbnail>
+<fanart>http://cdn.phoenixrebornbuild.com.hr/slike-phoenix/_phoenix-adult.jpg</fanart>
+<link>http://ignoreme</link>
+<jsonrpc>plugin://plugin.video.phoenixrebornfilmovizaodrasle</jsonrpc>
+</item>
+</items>
+
+<item>
+<title>[COLOR lightslategrey]----------------------------------------------------------------------[/COLOR]<title>
+<thumbnail>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</thumbnail>
+<fanart>https://i0.wp.com/www.broadbandtvnews.com/wp-content/uploads/2014/09/XBMC_screen.png</fanart>
+<link>https://goo.gl/Nz9ALW&amp;mode=showSite&amp;stopPlayback=yes</link>
+<item>
+
+<item>
+<title>[COLOR orangered]sport[/COLOR][COLOR white]365.[/COLOR][COLOR orangered]live[/COLOR]</title>
+<fanart>https://1.bp.blogspot.com/-1-7R17YvYcg/Ws8ybU6oUhI/AAAAAAAAgX4/CC4e0nFEBgINCcWQiHGKYGnv-PsGtX85ACLcBGAs/s1600/Sport365%2BLive%2BKodi%2B17%2BKrypton%2B1_e.png</fanart>
+<thumbnail>https://1.bp.blogspot.com/-1-7R17YvYcg/Ws8ybU6oUhI/AAAAAAAAgX4/CC4e0nFEBgINCcWQiHGKYGnv-PsGtX85ACLcBGAs/s1600/Sport365%2BLive%2BKodi%2B17%2BKrypton%2B1_e.png</thumbnail>
+<link>http://ignorame</link>
+<jsonrpc>plugin://plugin.video.sport365.live</jsonrpc>
+</item>
+</items>
+
+<item>
+<title>[COLOR khaki]DENIS LIST PLATINUM[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://s8.postimg.cc/q4zhuv6dx/icon.png</thumbnail>
+<fanart>https://s8.postimg.cc/q4zhuv6dx/icon.png</fanart>
+<externallink>http://kodi.csadria.org/DENIS/platinum1.xml</externallink>
+<item>
+
+<item>
+<title>[COLOR lime]Green[/COLOR][COLOR white] Addon[/COLOR]</title>
+<link>http://ignorame<link><thumbnail>http://marko.phoenixrebornbuild.com.hr/slike/grenicon.png</thumbnail>
+<fanart>http://marko.phoenixrebornbuild.com.hr/slike/grenfanart.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/nevena.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR crimson]=============[/COLOR][COLOR deepskyblue] TEST [/COLOR][COLOR crimson]=============[/COLOR]<title>
+<link>http://ignorame<link><thumbnail></thumbnail>
+<fanart></fanart>
+<externallink>http://</externallink>
+<item>
+
+<item>
+<title>[COLOR deepskyblue]TEST 1[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://bestiptv.site/wp-content/uploads/2016/10/24.jpg</thumbnail>
+<fanart>https://bestiptv.site/wp-content/uploads/2016/10/24.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/test.txt</externallink>
+<item>
+
+<item>
+<title>[COLOR deepskyblue]TEST 2[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://bestiptv.site/wp-content/uploads/2016/10/24.jpg</thumbnail>
+<fanart>https://bestiptv.site/wp-content/uploads/2016/10/24.jpg</fanart>
+<externallink>
+http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/test2.txt
+</externallink>
+<item> 
+
+<item>
+<title>[COLOR deepskyblue]TEST 3[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://bestiptv.site/wp-content/uploads/2016/10/24.jpg</thumbnail>
+<fanart>https://bestiptv.site/wp-content/uploads/2016/10/24.jpg</fanart>
+<externallink>https://pastebin.com/vysvm4KN</externallink>
+<item>                                                   
+
+<item>
+<title>[COLOR deepskyblue]TEST 4[/COLOR]<title>
+<link>http://ignorame<link><thumbnail>https://bestiptv.site/wp-content/uploads/2016/10/24.jpg</thumbnail>
+<fanart>https://bestiptv.site/wp-content/uploads/2016/10/24.jpg</fanart>
+<externallink>http://cdn.phoenixrebornbuild.com.hr/addons/phoenix_freetv_ADDON/test4.txt</externallink>
+<item> 												  
