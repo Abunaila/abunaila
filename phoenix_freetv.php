52dfa7979001b19b7282bee0c4378dc115785142
Abunaila <45642023+Abunaila@users.noreply.github.com>
2019-04-08 04:13:35 +0300

    Add files via upload

---
+++ phoenix_freetv.php
@@ -0,0 +1,252 @@
+This XML file does not appear to have any style information associated with it. The document tree is shown below.
+<item>
+<title>
+<![CDATA[ ottv.tk ]]>
+</title>
+<icon>
+<![CDATA[ http://ottv.tk/public/media/logo-light.png ]]>
+</icon>
+<background-image>
+<![CDATA[ http://ottv.tk/public/media/background.jpg ]]>
+</background-image>
+<items>
+<channel>
+<logo_30x30>
+</description>
+</channel>
+<channel>
+<logo_30x30>
+<![CDATA[ http://ottv.tk/media/logo-red.gif ]]>
+</logo_30x30>
+<playlist_url>
+http://iptvauto.com/auto.all.m3u
+</playlist_url>
+<title>
+<![CDATA[ AutoIPTV
+ ]]>
+</title>
+<description>
+<![CDATA[
+<br><center><img src="http://ottv.tk/media/logo-red.gif" width="22%"><br><hr><br> <br><br>В этом разделе закрепленные плейлисты <br>
+ если у вас есть свой плейлист и вы хотите поделиться им <br>напишите на ottv@ottv.tk
+ <br>
+]]>
+</description>
+</channel>
+<channel>
+<logo_30x30>
+<![CDATA[ http://ottv.tk/media/logo-red.gif ]]>
+</logo_30x30>
+<playlist_url>
+http://onlyiptv.info/iptv/OnlyWorkKIDS.m3u
+</playlist_url>
+<title>
+<![CDATA[ KIDS
+ ]]>
+</title>
+<description>
+<![CDATA[
+<br><center><img src="http://ottv.tk/media/logo-red.gif" width="22%"><br><hr><br> <br><br>В этом разделе закрепленные плейлисты <br>
+ если у вас есть свой плейлист и вы хотите поделиться им <br>напишите на ottv@ottv.tk
+ <br>
+]]>
+</description>
+</channel>
+<channel>
+<logo_30x30>
+<![CDATA[ http://ottv.tk/media/logo-red.gif ]]>
+</logo_30x30>
+<playlist_url>
+http://onlyiptv.info/iptv/OnlyWorkIPTV.m3u
+</playlist_url>
+<title>
+<![CDATA[ WorkIPTV
+ ]]>
+</title>
+<description>
+<![CDATA[
+<br><center><img src="http://ottv.tk/media/logo-red.gif" width="22%"><br><hr><br> <br><br>В этом разделе закрепленные плейлисты <br>
+ если у вас есть свой плейлист и вы хотите поделиться им <br>напишите на ottv@ottv.tk
+ <br>
+]]>
+</description>
+</channel>
+<channel>
+<logo_30x30>
+<![CDATA[ http://ottv.tk/media/logo-red.gif ]]>
+</logo_30x30>
+<playlist_url>
+http://onlyiptv.info/iptv/OnlyWorkMUZ.m3u
+</playlist_url>
+<title>
+<![CDATA[ WorkMUZ
+ ]]>
+</title>
+<description>
+<![CDATA[
+<br><center><img src="http://ottv.tk/media/logo-red.gif" width="22%"><br><hr><br> <br><br>В этом разделе закрепленные плейлисты <br>
+ если у вас есть свой плейлист и вы хотите поделиться им <br>напишите на ottv@ottv.tk
+ <br>
+]]>
+</description>
+</channel>
+<channel>
+<logo_30x30>
+<![CDATA[ http://ottv.tk/media/logo-red.gif ]]>
+</logo_30x30>
+<playlist_url>
+http://motivation.pp.ua/motivation.pp.ua.m3u
+</playlist_url>
+<title>
+<![CDATA[ motivation
+ ]]>
+</title>
+<description>
+<![CDATA[
+<br><center><img src="http://ottv.tk/media/logo-red.gif" width="22%"><br><hr><br> <br><br>В этом разделе закрепленные плейлисты <br>
+ если у вас есть свой плейлист и вы хотите поделиться им <br>напишите на ottv@ottv.tk
+ <br>
+]]>
+</description>
+</channel>
+<channel>
+<logo_30x30>
+<![CDATA[ http://ottv.tk/media/logo-red.gif ]]>
+</logo_30x30>
+<playlist_url>
+http://motivation.pp.ua/Stabil_05_18.m3u
+</playlist_url>
+<title>
+<![CDATA[ Stabil_05
+ ]]>
+</title>
+<description>
+<![CDATA[
+<br><center><img src="http://ottv.tk/media/logo-red.gif" width="22%"><br><hr><br> <br><br>В этом разделе закрепленные плейлисты <br>
+ если у вас есть свой плейлист и вы хотите поделиться им <br>напишите на ottv@ottv.tk
+ <br>
+]]>
+</description>
+</channel>
+<channel>
+<logo_30x30>
+
+]]>
+</description>
+</channel>
+<channel>
+<logo_30x30>
+<![CDATA[ http://ottv.tk/media/logo-red.gif ]]>
+</logo_30x30>
+<playlist_url>
+http://phoenixrebornbuild.com.hr/lists/pvr.m3u8
+</playlist_url>
+<title>
+<![CDATA[ AutoIPTV
+ ]]>
+</title>
+<description>
+<![CDATA[
+<br><center><img src="http://ottv.tk/media/logo-red.gif" width="22%"><br><hr><br> <br><br>В этом разделе закрепленные плейлисты <br>
+ если у вас есть свой плейлист и вы хотите поделиться им <br>напишите на ottv@ottv.tk
+ <br>
+]]>
+</description>
+</channel>
+<channel>
+<logo_30x30>
+<![CDATA[ http://ottv.tk/media/logo-red.gif ]]>
+</logo_30x30>
+<playlist_url>
+https://raw.githubusercontent.com/teamblue65/LISTAS/master/LISTA_PVR_SIMPLE_CLIENT
+</playlist_url>
+<title>
+<![CDATA[ KIDS
+ ]]>
+</title>
+<description>
+<![CDATA[
+<br><center><img src="http://ottv.tk/media/logo-red.gif" width="22%"><br><hr><br> <br><br>В этом разделе закрепленные плейлисты <br>
+ если у вас есть свой плейлист и вы хотите поделиться им <br>напишите на ottv@ottv.tk
+ <br>
+]]>
+</description>
+</channel>
+<channel>
+<logo_30x30>
+<![CDATA[ http://ottv.tk/media/logo-red.gif ]]>
+</logo_30x30>
+<playlist_url>
+https://raw.githubusercontent.com/teamblue65/LISTAS/master/LISTA_SSIPTV
+</playlist_url>
+<title>
+<![CDATA[ WorkIPTV
+ ]]>
+</title>
+<description>
+<![CDATA[
+<br><center><img src="http://ottv.tk/media/logo-red.gif" width="22%"><br><hr><br> <br><br>В этом разделе закрепленные плейлисты <br>
+ если у вас есть свой плейлист и вы хотите поделиться им <br>напишите на ottv@ottv.tk
+ <br>
+]]>
+</description>
+</channel>
+<channel>
+<logo_30x30>
+<![CDATA[ http://ottv.tk/media/logo-red.gif ]]>
+</logo_30x30>
+<playlist_url>
+https://pastebin.com/raw/PdcbWq1R
+</playlist_url>
+<title>
+<![CDATA[ WorkMUZ
+ ]]>
+</title>
+<description>
+<![CDATA[
+<br><center><img src="http://ottv.tk/media/logo-red.gif" width="22%"><br><hr><br> <br><br>В этом разделе закрепленные плейлисты <br>
+ если у вас есть свой плейлист и вы хотите поделиться им <br>напишите на ottv@ottv.tk
+ <br>
+]]>
+</description>
+</channel>
+<channel>
+<logo_30x30>
+<![CDATA[ http://ottv.tk/media/logo-red.gif ]]>
+</logo_30x30>
+<playlist_url>
+http://motivation.pp.ua/motivation.pp.ua.m3u
+</playlist_url>
+<title>
+<![CDATA[ motivation
+ ]]>
+</title>
+<description>
+<![CDATA[
+<br><center><img src="http://ottv.tk/media/logo-red.gif" width="22%"><br><hr><br> <br><br>В этом разделе закрепленные плейлисты <br>
+ если у вас есть свой плейлист и вы хотите поделиться им <br>напишите на ottv@ottv.tk
+ <br>
+]]>
+</description>
+</channel>
+<channel>
+<logo_30x30>
+<![CDATA[ http://ottv.tk/media/logo-red.gif ]]>
+</logo_30x30>
+<playlist_url>
+http://motivation.pp.ua/Stabil_05_18.m3u
+</playlist_url>
+<title>
+<![CDATA[ Stabil_05
+ ]]>
+</title>
+<description>
+<![CDATA[
+<br><center><img src="http://ottv.tk/media/logo-red.gif" width="22%"><br><hr><br> <br><br>В этом разделе закрепленные плейлисты <br>
+ если у вас есть свой плейлист и вы хотите поделиться им <br>напишите на ottv@ottv.tk
+ <br>
+]]>
+</description>
+</channel>
+<channel>
+<logo_30x30>
